b8fb3af74607c392c16ab070be01215440591e00
smyles96 <smyles96@vt.edu>
2021-05-01 18:25:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Alphabetizer
+
+### Requirements to Run the Code
+* Python 3.x is installed on your system
+* The `python3` execuatble is in your systenm path
+
+### Obtaining and Running the Code
+1. Clone this repository
+
+    ```bash
+    git clone https://github.com/smyles96/vt-alphabetizer.git
+    ```
+
+2. Change into its directory
+
+    ```bash
+    cd vt-alphabetizer
+    ```
+
+3. Run the main alphabetizer.py script to start an interactive terminal prompt
+
+    ```bash
+    # On Linux
+    chmod +x ./alphabetizer.py
+    ./alphabetizer.py
+
+    # On Windows
+    python3 alphabetizer.py
+    ```
+
+4. The program will continually ask for a string to alphabetize. After inputting one the program will output the letters in alphabetized order.
+
+    ```bash
+    ./alphabetize.py
+
+    Enter a string to alphabetize (or CTRL-C to stop): Virginia Tech
+    aceghiiinrTV
+    Enter a string to alphabetize (or CTRL-C to stop): 3 Blind Mice
+    BcdeiilMn
+    Enter a string to alphabetize (or CTRL-C to stop): Turtle
+    elrTtu
+    Enter a string to alphabetize (or CTRL-C to stop): <CTRL-C>
+    Exiting...
+    ```
+
+5. To quit, press CTRL+C
+6. For help, run the script with extra arguments (ex. `./alphabetize.py help`)
---
+++ alphabetizer.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+def _merge_sort(input_string):
+    ''' Perform a stable recursive merge sort on the given string '''
+
+    sublist_len = len(input_string)
+
+    # Base cases
+    if sublist_len == 0:
+        # Empty string was entered
+        return ''
+    elif sublist_len == 1:
+        # Sublist with one element reached
+
+        # Count non-alphanumeric characters as empty strings.
+        return input_string if input_string[0].isalpha() else ''
+    
+    # Split the sublist recursively
+    mid_index = sublist_len // 2
+    
+    left_side = _merge_sort(input_string[:mid_index])
+    right_side = _merge_sort(input_string[mid_index:])
+
+    # Now merge both sublists
+    merged = ""
+
+    left_index = 0
+    right_index = 0
+
+    left_side_len = len(left_side)
+    right_side_len = len(right_side)
+
+    # Loop until either sublist reaches its last index
+    while left_index < left_side_len and right_index < right_side_len:
+        left_char = left_side[left_index].lower()
+        right_char = right_side[right_index].lower()
+
+        if left_char <= right_char:
+            # Left side char comes before, or is same as, right side
+            merged += left_side[left_index]
+            left_index += 1
+        else:
+            # Right side char comes before left
+            merged += right_side[right_index]
+            right_index += 1
+
+    # Append any remaining elements one of the sublists may have
+    while left_index < left_side_len:
+        merged += left_side[left_index]
+        left_index += 1
+
+    while right_index < right_side_len:
+        merged += right_side[right_index]
+        right_index += 1
+
+    return merged
+
+def alphabetize(input_string):
+    ''' Alphabetize the characters in the given string using merge sort '''
+    return _merge_sort(input_string)
+
+#def alphabetize(input_str):
+#    ''' Alphabetize the characters in the given string using TimSort '''
+#    return "".join(
+#        sorted([char for char in input_str if char.isalpha()], key=str.lower)
+#    )
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        print('''
+        Alphabetizer Program
+        Usage:
+            ./alphabetizer.py
+        Summary:
+            Starts the program and continues to ask for strings
+            to alphabetize until CTRL-C is pressed.
+        Returns:
+            Upon entering a string, the program outputs the
+            letters of that string in alphabetical order.
+
+            Non-letter characters are discarded in the
+            process.
+        Examples:
+            ./alphabetizer.py
+               Enter a string to alphabetize (or CTRL-C to stop): Virginia Tech
+               aceghiiinrTV
+               Enter a string to alphabetize (or CTRL-C to stop): 3 Blind Mice
+               BcdeiilMn
+               Enter a string to alphabetize (or CTRL-C to stop): Turtle
+               elrTtu
+               Enter a string to alphabetize (or CTRL-C to stop): <CTRL-C>
+               Exiting...
+        ''')
+
+        exit(0)
+
+    try:
+        while True:
+            input_str = input('Enter a string to alphabetize (or CTRL-C to stop): ')
+            print( alphabetize(input_str) )
+    except KeyboardInterrupt:
+        print("\nExiting...")
